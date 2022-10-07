e0580cc433a37d5b2713822e681d6f0eac866563
nese <neseispir2@gmail.com>
2022-10-07 17:59:40 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+import "./index.css";
+import Homepage from "./pages/homepage";
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage/>
+    </div>
+  )
+  }
+export default App;
